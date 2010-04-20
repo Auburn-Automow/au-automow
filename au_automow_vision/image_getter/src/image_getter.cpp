5da3131d09eb5ddc8f4428033b4e7e3140539176
William Woodall <wjwwood@gmail.com>
2010-04-20 18:22:08 +0000

    Updated the line_processing node to publish the processed image.

--- au_automow_vision/image_getter/src/image_getter.cpp
+++ au_automow_vision/image_getter/src/image_getter.cpp
@@ -7,29 +7,33 @@
 using namespace cv;
 
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "image_getter");
-  ros::NodeHandle n;
-  ros::Publisher img_pub = n.advertise<sensor_msgs::Image>("image", 10);
-  ros::Rate loop_rate(5);
-
-  VideoCapture cap(0);
-  if(!cap.isOpened()) return -1;
-  Mat frame;
-  IplImage* img; 
-
-  while(ros::ok()) {
-    std::cout << "i" << std::endl;
-    cap >> frame;
-    img = new IplImage(frame);
-    sensor_msgs::Image::Ptr ros_img = sensor_msgs::CvBridge::cvToImgMsg(img);
-    sensor_msgs::Image msg(*ros_img);
-
-    img_pub.publish(msg);
-
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-
-  return 0;
+    ros::init(argc, argv, "image_getter");
+    ros::NodeHandle n;
+    ros::Publisher img_pub = n.advertise<sensor_msgs::Image>("image", 1);
+    ros::Rate loop_rate(5);
+    
+    VideoCapture cap(0);
+    if(!cap.isOpened()) {
+        ROS_WARN("No camera detected");
+        return -1;
+    }
+    Mat frame;
+    IplImage* img;
+    
+    while(ros::ok()) {
+        cap >> frame;
+        img = new IplImage(frame);
+        IplImage* out = cvCreateImage( cvSize(img->width/2,img->height/2), img->depth, img->nChannels );
+        cvPyrDown( img, out );
+        sensor_msgs::Image::Ptr ros_img = sensor_msgs::CvBridge::cvToImgMsg(out);
+        sensor_msgs::Image msg(*ros_img);
+        
+        img_pub.publish(msg);
+        
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+    
+    return 0;
 }
 
