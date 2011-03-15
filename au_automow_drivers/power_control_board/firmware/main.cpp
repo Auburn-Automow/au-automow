1ab83eccd7736915e1c0e9ece0167f1dcef9b64e
Michael Carroll <carroll.michael@gmail.com>
2011-03-15 04:49:03 -0500

    As far as I can get on firmware without hardware.

--- au_automow_drivers/power_control_board/firmware/main.cpp
+++ au_automow_drivers/power_control_board/firmware/main.cpp
@@ -10,8 +10,24 @@ ros::Publisher pcb_node;
 avr_bridge::PowerControl pcb_msg;
 avr_bridge::CutterControl cc_msg;
 
-OneWire oneWireTop(9);
-OneWire oneWireBot(10);
+// Digital IO
+#define pin_leftCutterCheck         2
+#define pin_rightCutterCheck        3
+#define pin_leftCutterControl       4
+#define pin_rightCutterControl      5
+#define pin_ledHigh                 6
+#define pin_ledMid                  7
+#define pin_ledLow                  8
+// One-Wire Bus Pins
+#define pin_temperatureTop          9
+#define pin_temperatureBot          10
+// Analog Inputs
+#define pin_voltage                 0
+#define pin_current                 1
+
+
+OneWire oneWireTop(pin_temperatureTop);
+OneWire oneWireBot(pin_temperatureBot);
 DallasTemperature temperatureTop(&oneWireTop);
 DallasTemperature temperatureBot(&oneWireBot);
 
@@ -22,28 +38,26 @@ namespace ros {
     }
 }
 
-void toggle()
-{ //toggle an led to debug the program
-    static char t=0;
-    if (!t ) {
-        digitalWrite(13, HIGH);   // set the LED on
-        t = 1;
-    } else {
-        digitalWrite(13, LOW);    // set the LED off
-        t = 0;
-    }
-}
-
 void cuttercallback(ros::Msg const *msg)
 {
-    toggle();
+    digitalWrite(13,cc_msg.LeftControl);
+    digitalWrite(pin_rightCutterControl,cc_msg.RightControl);
 }
 
 void setup()
 {
     Serial.begin(57600);
     pinMode(13,OUTPUT);
-   
+
+    // Set up all of the Digital IO pins.
+    pinMode(pin_leftCutterCheck,INPUT);
+    pinMode(pin_rightCutterCheck,INPUT);
+    pinMode(pin_leftCutterControl,OUTPUT);
+    pinMode(pin_rightCutterControl,OUTPUT);
+    pinMode(pin_ledHigh,OUTPUT);
+    pinMode(pin_ledMid,OUTPUT);
+    pinMode(pin_ledLow,OUTPUT);
+
     temperatureTop.begin();
     temperatureBot.begin();
 
@@ -59,13 +73,13 @@ void loop()
             break;
         node.spin(c);
     }
-    pcb_msg.Voltage = 24;
-    pcb_msg.Current = 10;
+    pcb_msg.Voltage = analogRead(pin_voltage);
+    pcb_msg.Current = analogRead(pin_current);
     pcb_msg.StateofCharge = 100;
     pcb_msg.Temperature1 = 10;
     pcb_msg.Temperature2 = 10;
-    pcb_msg.LeftCutterStatus = false;
-    pcb_msg.RightCutterStatus = true;
+    pcb_msg.LeftCutterStatus = digitalRead(pin_leftCutterCheck);
+    pcb_msg.RightCutterStatus = digitalRead(pin_rightCutterCheck);
     node.publish(pcb_node,&pcb_msg);
 
     delay(100);
