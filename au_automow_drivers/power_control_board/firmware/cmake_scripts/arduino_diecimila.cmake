4b1ba2453d347f6bc56a214304cde9404d1c4aab
William Woodall <wjwwood@gmail.com>
2011-04-11 13:45:14 -0500

    Changed power control board path

--- au_automow_drivers/power_control_board/firmware/cmake_scripts/arduino_diecimila.cmake
+++ au_automow_drivers/power_control_board/firmware/cmake_scripts/arduino_diecimila.cmake
@@ -3,6 +3,6 @@ set(ARDUINO_BOARD "atmega328p")
 set(ARDUINO_MCU "m328p")
 set(ARDUINO_FCPU "16000000")
 set(ARDUINO_UPLOAD_SPEED "57600")
-set(ARDUINO_PORT "/dev/ttyUSB0")
+set(ARDUINO_PORT "/dev/ttyUSB2")
 
 include(${CMAKE_SOURCE_DIR}/cmake_scripts/arduino.cmake)
