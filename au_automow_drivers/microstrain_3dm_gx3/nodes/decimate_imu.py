9a2b1c051671c099c51b5f757650b70c49c34bc4
William Woodall <wjwwood@gmail.com>
2011-05-23 10:48:30 -0500

    Working on 7state c++ filter.

--- au_automow_drivers/microstrain_3dm_gx3/nodes/decimate_imu.py
+++ au_automow_drivers/microstrain_3dm_gx3/nodes/decimate_imu.py
@@ -4,15 +4,15 @@ import rospy
 from sensor_msgs.msg import Imu
 
 imu_pub = None
-count = 0
+count = 1
 
 def callback(data):
     global imu_pub, count
-    if count == 10:
+    if count == 1:
         imu_pub.publish(data)
-        count = 0
+        count = 10
     else:
-        count += 1
+        count -= 1
 
 def listener():
     global imu_pub
